3c40ebf34dd3139a8c90c0a74c243d1bda97de09
Siva Dinesh .k <ksd.ccbp4@gmail.com>
2024-06-15 10:33:07 +0530

    not needed

---
+++ src/components/buttons/Buttons.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Buttons.css'
+
+const Buttons = () => {
+	return (
+		<div className='btnWrapper'>
+			<button type='button' className='btn outlined'>
+				Something
+			</button>
+		</div>
+	)
+}
+
+export default Buttons
